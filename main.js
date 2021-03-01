d37c6a3babfbd5b21b8834885ba716a60efc78a3
mfestrada <michaelxestrada@gmail.com>
2021-03-01 09:31:28 -0800

    adds timestamps and colors to console

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
 const fetch = require('node-fetch')
+const chalk = require('chalk')
 const client_id = (process.env.CLIENT_ID) ? process.env.CLIENT_ID : undefined
 const client_secret = (process.env.CLIENT_SECRET) ? process.env.CLIENT_SECRET : undefined
 const spotify_user_endpoint = (process.env.SPOTIFY_USER_ENDPOINT) ? process.env.SPOTIFY_USER_ENDPOINT : undefined
 let TOKEN, USER
 
 if (!process.env.CLIENT_ID | !process.env.CLIENT_SECRET | !process.env.SPOTIFY_USER_ENDPOINT) {
-  console.error("ERROR! $CLIENT_ID, $CLIENT_SECRET and $SPOTIFY_USER_ENDPOINT are not set!")
-  process.exit(2)
+  console.error(chalk.redBright('ERROR! CLIENT_ID, CLIENT_SECRET or SPOTIFY_USER_ENDPOINT are not set!'));
+  process.exit(2);
 }
 
-console.log(`CLIENT_ID: ${client_id}`);
-console.log(`CLIENT_SECRET: ${client_secret}`);
-console.log(`SPOTIFY_USER_ENDPOINT: ${spotify_user_endpoint}`);
+console.log(chalk.cyan(`CLIENT_ID: ${client_id}\nCLIENT_SECRET: ${client_secret}\nSPOTIFY_USER_ENDPOINT: ${spotify_user_endpoint}`));
 
 // run script
 (async () => {
   try {
-    await main(spotify_user_endpoint)
+    await main(spotify_user_endpoint);
   } catch (e) {
-    console.error(e)
-    process.exit(99)
+    console.error(e);
+    process.exit(99);
   }
-})();
+})()
 
 async function main(spotify_user_url) {
-  TOKEN = await getAccessToken()
-  USER = await getUser(TOKEN, spotify_user_url)
-  console.log(`INFO: original follower total: ${USER.followers.total}`);
+  TOKEN = await getAccessToken();
+  USER = await getUser(TOKEN, spotify_user_url);
+  console.info(chalk.grey(`INFO : ${new Date(new Date().getTime())} : session start total: ${USER.followers.total}`));
 
   setInterval(async () => {
-    TOKEN = await getAccessToken()
+    TOKEN = await getAccessToken();
     await getFollowerTotal({
       access_token: TOKEN,
       url: spotify_user_url
-    })
-  }, 30000)
+    });
+  }, 30000);
 }
 
 async function getAccessToken() {
@@ -44,8 +43,8 @@ async function getAccessToken() {
       'Authorization' : 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
       'Content-Type' : 'application/x-www-form-urlencoded'
     }
-  })
-  return (await data.json()).access_token
+  });
+  return (await data.json()).access_token;
 }
 
 async function getData(access_token, url) {
@@ -54,23 +53,27 @@ async function getData(access_token, url) {
     headers: {
       'Authorization' : 'Bearer ' + access_token
     }
-  })
+  });
 }
 
 async function getUser(access_token, url) {
-  let resp = await getData(access_token, url)
-  let data = {...(await resp.json())}
-  return data
+  let resp = await getData(access_token, url);
+  let data = {...(await resp.json())};
+  return data;
 }
 
 async function getFollowerTotal(x) {
-  let latest = await getUser(x.access_token, x.url)
+  let latest = await getUser(x.access_token, x.url);
   if (USER.followers.total < latest.followers.total) {
-    USER = latest
-    console.log(`ADDED: ${USER.followers.total}`)
+    USER = latest;
+    console.log(chalk.green(`ADDED : ${getLogString()}`));
   }
   if (USER.followers.total > latest.followers.total) {
-    USER = latest
-    console.log(`REMOVED: ${USER.followers.total}`)
+    USER = latest;
+    console.log(chalk.red(`REMOVED : ${getLogString()}`));
   }
+}
+
+function getLogString() {
+  return `${new Date(new Date().getTime())} : ${USER.followers.total}`;
 }
